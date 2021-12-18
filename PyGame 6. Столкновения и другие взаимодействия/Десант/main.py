f5144cb3a07e17758bd0741d22f2c31fa458562e
st10-01-16 <artu.200505@gmail.com>
2021-12-18 16:42:38 +0500

    Десант

---
+++ PyGame 6. Столкновения и другие взаимодействия/Десант/main.py
@@ -0,0 +1,82 @@
+import os
+import sys
+import pygame
+
+
+pygame.init()
+size = width, height = (700,) * 2
+pygame.display.set_caption("Десант")
+screen = pygame.display.set_mode(size)
+screen.fill(pygame.Color("white"))
+
+
+def load_image(name, color_key=-1):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+
+    if color_key is not None:
+        image = image.convert()
+        if color_key == -1:
+            color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+class Mountain(pygame.sprite.Sprite):
+    image = load_image("mountains.png")
+
+    def __init__(self):
+        super().__init__(all_sprites)
+        self.image = Mountain.image
+        self.rect = self.image.get_rect()
+        # вычисляем маску для эффективного сравнения
+        self.mask = pygame.mask.from_surface(self.image)
+        # располагаем горы внизу
+        self.rect.bottom = height
+
+
+class Landing(pygame.sprite.Sprite):
+    image = load_image("pt.png")
+
+    def __init__(self, pos):
+        super().__init__(all_sprites)
+        self.image = Landing.image
+        self.rect = self.image.get_rect()
+        # вычисляем маску для эффективного сравнения
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+
+    def update(self):
+        if not pygame.sprite.collide_mask(self, mountain):
+            self.rect = self.rect.move(0, 1)
+
+
+fps = 60  # количество кадров в секунду
+clock = pygame.time.Clock()
+running = True
+
+all_sprites = pygame.sprite.Group()
+mountain = Mountain()
+
+all_sprites.draw(screen)
+
+while running:  # главный игровой цикл
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            Landing(event.pos)
+
+    all_sprites.update()
+    screen.fill(pygame.Color("white"))
+    all_sprites.draw(screen)
+    pygame.display.flip()  # смена кадра
+    # временная задержка
+    clock.tick(fps)
