10de89349a9b88d5c50ecdea1af5632097673f54
st10-01-16 <artu.200505@gmail.com>
2021-12-19 12:14:00 +0500

    just do it!

--- PyGame 6. Столкновения и другие взаимодействия/Boom them all — 2/main.py
+++ PyGame 6. Столкновения и другие взаимодействия/Boom them all — 2/main.py
@@ -31,26 +31,31 @@ class Bomb(pygame.sprite.Sprite):
     image_boom = load_image("boom.png")
 
     def __init__(self, *group):
-        # НЕОБХОДИМО вызвать конструктор родительского класса Sprite.
-        # Это очень важно!!!
         super().__init__(*group)
         self.image = Bomb.image
+        self.mask = pygame.mask.from_surface(Bomb.image)
         self.rect = self.image.get_rect()
-        self.rect.x = random.randrange(width)
-        self.rect.y = random.randrange(height)
+
+        def collide_any(obj):
+            for sprite in all_sprites:
+                if pygame.sprite.collide_mask(obj, sprite) and not (sprite is obj):
+                    return True
+
+        while collide_any(self):
+            self.rect.x = random.randrange(width - 80)
+            self.rect.y = random.randrange(height - 80)
 
     def update(self, *args):
-        self.rect = self.rect.move(random.randrange(3) - 1,
-                                   random.randrange(3) - 1)
         if args and args[0].type == pygame.MOUSEBUTTONDOWN and \
-            self.rect.collidepoint(args[0].pos):
+                self.rect.collidepoint(args[0].pos):
             self.image = self.image_boom
 
 
 screen = pygame.display.set_mode(size)
 all_sprites = pygame.sprite.Group()
-for _ in range(50):
+for _ in range(25):
     Bomb(all_sprites)
+
 running = True
 cords_mouse = None, None
 while running:
